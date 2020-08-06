aae27be04f393f1a44a0dfc88fc0ecb258e40315
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-08-06 18:46:09 +0800

    Add files via upload

---
+++ GetProxyInfo.c
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+FILE* proxy_ini;
+char proxy[30];
+
+int main() {
+	system("reg query \"HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" | find \"ProxyEnable\" > temp\\proxy.ini.0");
+	if (system("type temp\\proxy.ini.0 | find \"0x0\"") == 0) {
+		proxy_ini = fopen("config\\proxy.ini", "w");
+		fprintf(proxy_ini, "proxy=0");
+	}
+	else {
+		system("reg query \"HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" | find \"ProxyServer\" > temp\\proxy.ini.1");
+		proxy_ini = fopen("temp\\proxy.ini.1", "r");
+		fscanf(proxy_ini, "    ProxyServer    REG_SZ    %s", proxy);
+		fclose(proxy_ini);
+		system("del temp\\proxy.ini.1");
+		proxy_ini = fopen("config\\proxy.ini", "w");
+		fprintf(proxy_ini, "proxy=%s", proxy);
+	}
+	fclose(proxy_ini);
+	system("del temp\\proxy.ini.0");
+	return 0;
+}
