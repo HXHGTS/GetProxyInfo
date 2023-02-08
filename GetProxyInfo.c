eb773690ec2e5b204b487a2e3b34e6f420e60d28
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-02-08 18:45:00 +0800

    Add files via upload

--- GetProxyInfo.c
+++ GetProxyInfo.c
@@ -4,9 +4,8 @@
 
 FILE* proxy_ini;
 int ReturnValue,Num;
-char temp[100], proxy[23],proxy_source[80];
+char temp[120], proxy[23],proxy_source[80];
 char http_proxy[22], https_proxy[22], ftp_proxy[22];
-
 int main() {
 	if (_access("config", 0) == -1) {
 		system("mkdir config");
@@ -23,15 +22,25 @@ int main() {
 	else {
 		system("reg query \"HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" | find \"ProxyServer\" > temp\\proxy.txt");//检测一般代理
 		proxy_ini = fopen("temp\\proxy.txt", "r");
-		ReturnValue=fscanf(proxy_ini, "    ProxyServer    REG_SZ    %s", temp);
+		ReturnValue=fscanf(proxy_ini, "    ProxyServer    REG_SZ    %s", temp);//http=127.0.0.1:8888;https=127.0.0.1:8888;ftp=127.0.0.1:8888
 		fclose(proxy_ini);
 		if (temp[0] == 'h') {
-			system("sed -i \"s/;/ /g\" temp\\proxy.txt");
-			system("sed -i \"s/http=//g\" temp\\proxy.txt");
-			system("sed -i \"s/https=//g\" temp\\proxy.txt");
-			system("sed -i \"s/ftp=//g\" temp\\proxy.txt");
-			proxy_ini = fopen("temp\\proxy.txt", "r");
-			ReturnValue = fscanf(proxy_ini, "    ProxyServer    REG_SZ %s %s %s", http_proxy,https_proxy,ftp_proxy);
+			for (Num = 0; Num <= 120; Num++) {
+				if (temp[Num] == ';') {
+					temp[Num] = ' ';
+				}
+				else if (temp[Num] == '=') {//http 127.0.0.1:8888 https 127.0.0.1:8888 ftp 127.0.0.1:8888
+					temp[Num] = ' ';
+				}
+				else if (temp[Num] == '\0') {
+					break;
+				}
+			}
+			proxy_ini = fopen("temp\\proxy.txt.out", "w");
+			ReturnValue = fprintf(proxy_ini, "%s", temp);//http 127.0.0.1:8888 https 127.0.0.1:8888 ftp 127.0.0.1:8888
+			fclose(proxy_ini);
+			proxy_ini = fopen("temp\\proxy.txt.out", "r");
+			ReturnValue = fscanf(proxy_ini, "http %s https %s ftp %s",http_proxy,https_proxy,ftp_proxy);
 			fclose(proxy_ini);
 			proxy_ini = fopen("config\\proxy.ini", "w");
 			fprintf(proxy_ini, "proxy=%s", http_proxy);
@@ -46,10 +55,8 @@ int main() {
 			fclose(proxy_ini);
 		}
 	}
-	system("move /Y sed.exe se.bak");
-	system("del /f /s /q sed*");
-	system("move /Y se.bak sed.exe");
 	system("del /F /S /Q temp\\proxy.txt");
+	system("del /F /S /Q temp\\proxy.txt.out");
 	system("del /F /S /Q temp\\proxyswitcher.temp");
 	system("cls");
 	return 0;
